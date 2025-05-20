6ee63f6fd380209900d8fa01ca17aa543cbdb13c
pauldecalf <nauticblumusique@gmail.com>
2025-05-20 22:15:43 +0200

    Modification des title et meta description

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Metadata } from 'next'
 import { Header } from './components/Header'
 import { HeroSection } from './components/HeroSection'
 import { ServicesSection } from './components/ServicesSection'
@@ -9,6 +10,11 @@ import { PortfolioSection } from './components/PortfolioSection'
 import { ContactSection } from './components/ContactSection'
 import { FaqSection } from './components/FaqSection'
 import { Footer } from './components/Footer'
+
+export const metadata: Metadata = {
+  title: "Création de sites web sur-mesure pour votre entreprise",
+  description: "Des sites internet professionnels et optimisés pour PME, indépendants et artisans. Design moderne qui convertit vos visiteurs en clients.",
+};
 
 export default function Home() {
   return (
